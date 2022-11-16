7369ad4e88177b702d8ad863e8d9a7c2c6e51165
Francis <FrancisJY.Hu@moxa.com>
2022-11-16 15:19:38 +0800

    Modify to zsh based script.
     Changes to be committed:
    	modified:   show_runningcfg.sh

--- show_runningcfg.sh
+++ show_runningcfg.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 if [ -z "$1" ]; then
 	echo "Usage: show_runningcfg.sh [cfg.json]"
 	echo "Exmaple:" 
 	echo "	show_runningcfg.sh mxpsec"
-elif [ "$1" == "all" ]; then
-    jsons=($(sudo ls /moxa/run/flask-moxa-config-layer/RunningConfig/))
+elif [ "$1" = "all" ]; then
+    jsons=($(ls /moxa/run/flask-moxa-config-layer/RunningConfig/))
     count=${#jsons[@]}
     touch all_cfg
     cat /dev/null > all_cfg
     for ((i=0;i<count;i++)); do 
         echo "[${jsons[i]}]" >> all_cfg
-        sudo python -m json.tool /moxa/run/flask-moxa-config-layer/RunningConfig/${jsons[i]} >> all_cfg
+        python -m json.tool /moxa/run/flask-moxa-config-layer/RunningConfig/${jsons[i]} >> all_cfg
     done
 else
-	sudo python -m json.tool /moxa/run/flask-moxa-config-layer/RunningConfig/$1.json
+	python -m json.tool /moxa/run/flask-moxa-config-layer/RunningConfig/$1.json
 fi
